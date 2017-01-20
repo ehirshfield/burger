8fe7367787abb4b7e4842654db7e4fb9fa9dedca
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2017-01-20 00:14:30 -0800

    Finished style, need to upload to heroku

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require("express");
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require("express-handlebars");
+var path = require("path");
 
 var app = express();
 var PORT = 8080;
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(methodOverride("_method"));
