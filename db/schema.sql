e13cc3009603899ca313874956f782808e14952c
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2017-01-21 09:11:44 -0800

    removed php and composer.json to test

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
-  id INT NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(255) NOT NULL,
-  devoured BOOLEAN NOT NULL DEFAULT 0,
-  ts TIMESTAMP,
-	PRIMARY KEY (id)
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean NOT NULL DEFAULT 0,
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
 );
