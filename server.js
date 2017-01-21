0600a466b665fe04d24b913cd48afd25ff38a928
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2017-01-21 09:47:02 -0800

    changed port to work on heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 var path = require("path");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
