bbc203d71500d1853763f90c22117512c264d7fa
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2017-01-20 22:39:41 -0800

    setting up JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burger_db"
+  });
+};
 
 module.exports = connection;
